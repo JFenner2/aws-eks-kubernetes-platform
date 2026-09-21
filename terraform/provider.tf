c00193f15109e5234413b38dacc0461facaf83a0
Josh Fenner <joshfenner.8@gmail.com>
2026-09-21 19:38:26 +1000

    manage EKS cluster bootstrap with Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -20,3 +20,9 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.main.token
   }
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
